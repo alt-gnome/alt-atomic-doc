0a7e1ed124526c746126a2fb30e00881d6db0c8f
Олег Щавелев <o.shchavelev@yandex.ru>
2025-01-26 03:25:16 +0300

    docs: add footer, add licence MIT

--- .vitepress/config/ru.ts
+++ .vitepress/config/ru.ts
@@ -1,4 +1,5 @@
 import { defineConfigWithTheme, type DefaultTheme } from 'vitepress'
+import { packages } from '../../package-lock.json'
 
 export const nav = (): DefaultTheme.NavItem[] => [{ text: 'Главная', link: '/' }]
 
@@ -6,6 +7,13 @@ export const ru = defineConfigWithTheme<DefaultTheme.Config>({
   lang: 'ru-RU',
   description: 'Атомарный облачный дистрибутив на базе новейших технологий',
   themeConfig: {
-    nav: nav()
+    nav: nav(),
+    footer: {
+      message: 'Содержание доступно <a href="/licence.html">по лицензии MIT</a>',
+      copyright: `
+                  ${new Date().getFullYear()} ALT Atomic Wiki,
+                  разработано на платформе <a href="//vitepress.dev/">VitePress ${packages['node_modules/vitepress'].version}</a>
+                `
+    }
   }
 })
